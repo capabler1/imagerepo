5d3744540447248f99b937db91f1226d0d5ff7f7
capabler1 <761080344@qq.com>
2022-03-31 23:33:47 +0800

    php

--- webp/a.py
+++ webp/a.py
@@ -7,5 +7,6 @@ txt = os.listdir(filePath)
 
 file_handle = open('F:\gitrepo\imagerepo\webp\webp.txt', mode='w')
 for i in range(len(txt)):
-    file_handle.write("https://cdn.jsdelivr.net/gh/capabler1/imagerepo@1.0/webp/"+txt[i-1]+"\n")
+    file_handle.write(
+        "https://cdn.jsdelivr.net/gh/capabler1/imagerepo@1.0/webp/"+txt[i-1]+"\n")
 file_handle.close()

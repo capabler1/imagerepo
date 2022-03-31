ea4a2abb2d9ff4828329be3fab729bc9fb63c5e4
capabler1 <761080344@qq.com>
2022-03-31 23:43:26 +0800

    api

--- webp/random.php
+++ webp/random.php
@@ -1,21 +1,6 @@
+
 <?php
-//存有美图链接的文件名img.txt
-$filename = "webp.txt";
-if(!file_exists($filename)){
-    die('文件不存在');
-}
-
-//从文本获取链接
-$pics = [];
-$fs = fopen($filename, "r");
-while(!feof($fs)){
-    $line=trim(fgets($fs));
-    if($line!=''){
-        array_push($pics, $line);
-    }
-}
-
-//从数组随机获取链接
-$pic = $pics[array_rand($pics)];
-die(header("Location: $pic"));
+$img=file('webp.txt');
+$url=array_rand($img);
+header("Location:".$img[$url]);
 ?>

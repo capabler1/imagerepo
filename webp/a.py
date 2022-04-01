306903b42d667fb1e283d849ded7bbacce108a9d
capabler1 <761080344@qq.com>
2022-04-02 01:12:16 +0800

    js api

--- webp/a.py
+++ webp/a.py
@@ -7,6 +7,9 @@ txt = os.listdir(filePath)
 
 file_handle = open('F:\gitrepo\imagerepo\webp\webp.txt', mode='w')
 for i in range(len(txt)):
-    file_handle.write(
-        "https://cdn.jsdelivr.net/gh/capabler1/imagerepo@1.0/webp/"+txt[i-1]+"\n")
+    # file_handle.write("var a"+str(i-1)+'="'
+    #     "https://cdn.jsdelivr.net/gh/capabler1/imagerepo@1.0/webp/"+txt[i-1]+'"'+"\n")
+        file_handle.write('"'
+        "https://cdn.jsdelivr.net/gh/capabler1/imagerepo@1.0/webp/"+txt[i-1]+'"'+",")
+
 file_handle.close()

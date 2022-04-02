0c402e8b94bd487cefde93f5bca9eb3aae807c06
capabler1 <761080344@qq.com>
2022-04-02 13:42:26 +0800

    jss

--- webpimage/a.py
+++ webpimage/a.py
@@ -1,5 +1,5 @@
 import os
-filePath = 'F:\gitrepo\imagerepo\webp'
+filePath = 'F:\gitrepo\imagerepo\webpimage'
 os.listdir(filePath)
 # print(os.listdir(filePath))
 txt = os.listdir(filePath)
@@ -9,7 +9,6 @@ file_handle = open('F:\gitrepo\imagerepo\webpimage\webp.txt', mode='w')
 for i in range(len(txt)):
     # file_handle.write("var a"+str(i-1)+'="'
     #     "https://cdn.jsdelivr.net/gh/capabler1/imagerepo@1.0/webp/"+txt[i-1]+'"'+"\n")
-    file_handle.write(
-        '"'+"https://cdn.jsdelivr.net/gh/capabler1/imagerepo@1.0/webp/"+txt[i-1]+'"'+",")
+    file_handle.write('"'+"https://cdn.jsdelivr.net/gh/capabler1/imagerepo@1.0/webp/"+txt[i-1]+'"'+",")
 
 file_handle.close()

52e2464d61f25450ccfe4c751ded84e60032dd7c
capabler1 <761080344@qq.com>
2022-04-03 23:39:47 +0800

    boke

--- webpimage/a.py
+++ webpimage/a.py
@@ -9,6 +9,7 @@ file_handle = open('F:\gitrepo\imagerepo\webpimage\webp.txt', mode='w')
 for i in range(len(txt)):
     # file_handle.write("var a"+str(i-1)+'="'
     #     "https://cdn.jsdelivr.net/gh/capabler1/imagerepo@1.0/webp/"+txt[i-1]+'"'+"\n")
-    file_handle.write('"'+"https://cdn.jsdelivr.net/gh/capabler1/imagerepo@1.0/webp/"+txt[i-1]+'"'+",")
+    file_handle.write(
+        '![]('+"https://cdn.jsdelivr.net/gh/capabler1/imagerepo@1.0/webp/"+txt[i-1]+')'+"\n")
 
 file_handle.close()
